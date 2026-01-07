bfb3587a942886355edf2ac9dff6b86600c6ef4c
S0umyajit <soumyajit2k18@gmail.com>
2026-01-08 00:34:40 +0530

    Third Commmit

--- src/main/java/com/example/springweb/controllers/EmployeeController.java
+++ src/main/java/com/example/springweb/controllers/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.springweb.controllers;
 
 import com.example.springweb.dto.EmployeeDto;
+import com.example.springweb.entities.EmployeeEntity;
+import com.example.springweb.repositories.EmployeeRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/employee")
@@ -14,25 +17,45 @@ public class EmployeeController {
 //    public String getEmployee(){
 //        return "HuHaha";
 //    }
+//
+//    @GetMapping(path = "/{empId}")
+//    public EmployeeDto getEmployeeById(@PathVariable(name="empId") Long id){
+//        return new EmployeeDto(id,"Soumyajit",26,"soumyajit2k18@gmail.com", LocalDate.of(2022,06,26));
+//    }
+//
+//    @GetMapping
+//    public String getEmployee(@RequestParam(required = false) Integer age,
+//                              @RequestParam(required = false) String name){
+//        return "Age is "+age +" ,name is: "+ name;
+//    }
+//
+//    @PostMapping
+//    public EmployeeDto createEmp( @RequestBody EmployeeDto employeeDto){
+//        employeeDto.setEmpId(1L);
+//        return employeeDto;
+//    }
+//    @PutMapping
+//    public String updateEmp(){
+//        return "Hello from put";
+//    }
 
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository){
+        this.employeeRepository=employeeRepository;
+    }
     @GetMapping(path = "/{empId}")
-    public EmployeeDto getEmployeeById(@PathVariable(name="empId") Long id){
-        return new EmployeeDto(id,"Soumyajit",26,"soumyajit2k18@gmail.com", LocalDate.of(2022,06,26));
+    public EmployeeEntity getEmployeeById(@PathVariable(name="empId") Long id){
+        return employeeRepository.findById(id).orElse(null);
     }
 
     @GetMapping
-    public String getEmployee(@RequestParam(required = false) Integer age,
-                              @RequestParam(required = false) String name){
-        return "Age is "+age +" ,name is: "+ name;
+    public List<EmployeeEntity> getEmployee(@RequestParam(required = false) Integer age,
+                                            @RequestParam(required = false) String name){
+        return employeeRepository.findAll();
     }
-
     @PostMapping
-    public EmployeeDto createEmp( @RequestBody EmployeeDto employeeDto){
-        employeeDto.setEmpId(1L);
-        return employeeDto;
-    }
-    @PutMapping
-    public String updateEmp(){
-        return "Hello from put";
+    public EmployeeEntity createEmp( @RequestBody EmployeeEntity employeeEntity) {
+        return employeeRepository.save(employeeEntity);
     }
 }
