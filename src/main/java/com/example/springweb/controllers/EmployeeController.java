c1ac869fe61681d20a5ea0642267a0ef548cbf01
S0umyajit <soumyajit2k18@gmail.com>
2026-01-07 21:27:44 +0530

    Second Commmit

--- src/main/java/com/example/springweb/controllers/EmployeeController.java
+++ src/main/java/com/example/springweb/controllers/EmployeeController.java
@@ -1,29 +1,38 @@
 package com.example.springweb.controllers;
 
 import com.example.springweb.dto.EmployeeDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/employee")
 public class EmployeeController {
 
-    @GetMapping(path = "/getEmployee")
-    public String getEmployee(){
-        return "HuHaha";
+//    @GetMapping(path = "/getEmployee")
+//    public String getEmployee(){
+//        return "HuHaha";
+//    }
+
+    @GetMapping(path = "/{empId}")
+    public EmployeeDto getEmployeeById(@PathVariable(name="empId") Long id){
+        return new EmployeeDto(id,"Soumyajit",26,"soumyajit2k18@gmail.com", LocalDate.of(2022,06,26));
     }
 
-    @GetMapping(path = "/employees/{empId}")
-    public EmployeeDto getEmployeeById(@PathVariable Long empId){
-        return new EmployeeDto(empId,"Soumyajit",26,"soumyajit2k18@gmail.com", LocalDate.of(2022,06,26));
-    }
-
-    @GetMapping(path ="/employees")
+    @GetMapping
     public String getEmployee(@RequestParam(required = false) Integer age,
                               @RequestParam(required = false) String name){
         return "Age is "+age +" ,name is: "+ name;
+    }
+
+    @PostMapping
+    public EmployeeDto createEmp( @RequestBody EmployeeDto employeeDto){
+        employeeDto.setEmpId(1L);
+        return employeeDto;
+    }
+    @PutMapping
+    public String updateEmp(){
+        return "Hello from put";
     }
 }
